91b46f990169807cc6396950065598f125eed13f
Martin Cote <MARTIN.COTE.4@GMAIL.COM>
2023-03-24 22:29:28 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# image_generator
+# Image Generator
+
 Simple web UI for generating images using OpenAI Dall-E.
+
+<img src="./images/image_generator.jpg" height=500>
+
+# Using OpenAI costs credits !
+
+Generating images using the OpenAI API costs money. Make sure to buy credits before running this project. [2] 
+
+# Install
+
+1 - Get an API key from OpenAI. Buy OpenAI credits if you don't have credits.
+
+    https://platform.openai.com/docs/api-reference/introduction
+
+2 - Set your API key as an environment variable.
+
+    $ cat ~/.bashrc
+   
+    (...)
+    export OPENAI_TOKEN=YOUR_TOKEN
+
+3 - Source .bashrc.
+
+    $ source ~/.bashrc
+
+4 - Clone this repository.
+
+    $ cd ~
+    $ git clone martin0004/image_generator
+
+5 - Install miniconda.
+
+    https://docs.conda.io/en/latest/miniconda.html
+
+6 - Create environment for this project.
+
+    $ conda env create --file ~/image_generator/env/environment.yaml
+
+7 - Activate environment.
+
+    $ conda activate openai
+    (openai) $
+
+8 - Launch image generator.
+
+    (openai) $ python ~/image_generator/image_generator.py
+
+9 - Open your browser and type `localhost:8050` in the address bar.
+
+10 - Type a prompt, the number of images you want to generate, select the size of the image, then press "Submit". Voilà!
+
+(Remember that each run will cost you OpenAI credits! [2]).
+
+<br>
+<img src="./images/image_generator.jpg" height=500>
+<br>
+<br>
+
+11 - [OPTIONAL] Create alias for launching the app.
+
+    ```
+    $ cat .bashrc
+
+    (...)
+    alias openai='conda activate openai; \
+                  python3 ~/image_generator/image_generator.py'
+    ```
+
+
+# References
+
+[1] Plotly, Dash, https://dash.plotly.com/
+[2] OpenAI, API Reference - Authentication, https://platform.openai.com/docs/api-reference/authentication
+[3] OpenAI, API Reference - Images - Create image, https://platform.openai.com/docs/api-reference/images/create
---
+++ image_generator.py
@@ -0,0 +1,154 @@
+###############################################################################
+# IMPORTS                                                                     #
+###############################################################################
+
+# Standard libraries
+import os
+
+# Third-party libraries
+from dash import Dash, dcc, html, Input, Output, State
+import dash_bootstrap_components as dbc
+import openai
+import plotly.graph_objects as go
+
+# User-defined libraries
+# (none...)
+
+###############################################################################
+# APP                                                                         #
+###############################################################################
+
+app = Dash(__name__,
+           external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+###############################################################################
+# LAYOUT                                                                      #
+###############################################################################
+
+title = html.H1("OpenAI Image Generator")
+
+prompt = html.Div(
+    [
+        dbc.Label("prompt"),
+        dbc.Textarea(id="prompt")
+    ]
+)
+
+n = html.Div(
+    [
+        dbc.Label("n"),
+        dbc.Input(type="number", min=0, max=10, step=1, id="n")
+    ]
+)
+
+size = html.Div(
+    [
+        dbc.Label("size"),
+        dcc.Dropdown(
+            [
+                "256x256",
+                "512x512",
+                "1024x1024",
+            ],
+            id="size"
+        )
+
+    ]
+)
+
+submit = html.Div(
+    [
+        html.Br(),
+        dbc.Button("Submit", id="submit")
+    ]
+)
+
+output = html.Div(id="output")
+
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(title)
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(prompt)
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(n)
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(size)
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(submit)
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(output)
+            ]
+        )
+    ],
+    fluid=True,
+    style = {
+        "height": "100vh"
+    }
+)
+
+
+###############################################################################
+# CALLBACKS                                                                   #
+###############################################################################
+
+@app.callback(
+    Output("output", "children"),
+    Input("submit", "n_clicks"),
+    State("prompt", "value"),
+    State("n", "value"),
+    State("size", "value")
+
+)
+def create_images(submit_n_clicks, prompt_value, n_value, size_value)-> html.Div:
+
+    output_children = []
+
+    # Ignore callback if app is loading for the 1st time
+
+    if submit_n_clicks is None:
+        return output
+
+    openai.api_key = os.environ["OPENAI_TOKEN"]
+
+    response = openai.Image.create(
+        prompt = prompt_value,
+        n = n_value,
+        size = size_value
+    )
+
+    for data in response["data"]:
+
+        url = str(data["url"])
+        img = html.Img(src = url)
+        output_children.append(img)
+
+    return output_children
+
+
+###############################################################################
+# MAIN                                                                        #
+###############################################################################
+
+if __name__ == "__main__":
+
+    app.run_server(debug=True)
+
+
